3ea7d682e8e99691cbdbe1284900b25e4e6b4e79
Luckyantony <luckyantonyofficial@gmail.com>
2025-06-26 16:53:41 +0300

    Updated models.py file

--- models.py
+++ models.py
@@ -1,14 +1,28 @@
 from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_hash = db.Column(db.String, nullable=False)
+
     vehicles = db.relationship("Vehicle", backref="user", cascade="all, delete")
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def to_dict(self):
-        return {"id": self.id, "username": self.username}
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
 
 class Vehicle(db.Model):
     __tablename__ = "vehicles"
